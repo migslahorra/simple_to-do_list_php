13e7b1794f726fcaf32a6d816567989e4ee2d0a2
Migs <lahorrajm@gmail.com>
2025-07-10 12:11:10 +0800

    added commented sql query syntax for the used database

---
+++ practice.sql
@@ -0,0 +1,8 @@
+/* Take note that this will only work for executing in the sql query tab in xampp*/
+/* CREATE DATABASE practice;
+
+CREATE TABLE tasks (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    task VARCHAR(55) NOT NULL,
+    complete INT(1) DEFAULT 0
+);
